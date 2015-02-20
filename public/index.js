9ffeffdfe77190737239eaef983f7e60d0967bea
unknown <bhushan_indian@hotmail.com>
2015-02-20 17:54:31 -0500

    updated ajax file

--- public/index.js
+++ public/index.js
@@ -1,22 +1,28 @@
 	$(document).ready(function(){
-			$('#button1').click(function(){
-				console.log("hello");
+			$('#button1').click(function(e){
+				//console.log("hello");
+				e.preventDefault();
 				var fromdata = {
 
 					'name' 			: $('input[name=firstname]').val(),
-					'middelname' 	: $('input[name=middelname]').val(),
-					'lastlname' 	: $('input[name=lastname]').val(),
-					'homephone' 	: $('input[name=homephone]').val()
+					'lastname' 		: $('input[name=lastname]').val(),
+					'homephone' 	: $('input[name=homephone]').val(),
+					'email' 		: $('input[name=email]').val(),
+
 				}
+				//var name1 = {name,lastname,homephone,email};
+				//console.log(fromdata)
 				$.ajax({
 					type	: 'post',
 					url		: '/users',
 					data	: fromdata,
-					datatype: 'json',
+					//datatype: 'json',
 					success	: function(res){
-						console.log(res)
+						console.log(res);
+						document.getElementById('on_page').innerHTML = res.name+'<br>'+res.lastname+'<br>'+res.homephone+'<br>'+res.email;
 					}   			
-
+					
+					
 				})
 
 			})
