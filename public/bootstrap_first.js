401302b33315b961b184c788aff7c6bf49da1d57
unknown <bhushan_indian@hotmail.com>
2015-02-28 12:46:05 -0500

    store exe  js change

--- public/bootstrap_first.js
+++ public/bootstrap_first.js
@@ -1,20 +1,22 @@
 	$(document).ready(function(){
 	console.log("hi")
-			$('#buton').click(function(){	
-			
-				console.log("hello");
+			$('#buton').click(function(event){	
+			event.preventDefault()
+				
 				var formdata ={
 					'name' 			: $('input[name=item]').val(),
 					'description'	: $('name[name=des]').val(),
 					'price'			: $('name[name=price]').val(),
 				}
+				console.log(formdata);
 				$.ajax({
-					type	: 'get',
-					url		: '/document',
-					data	: fromdata,
+					type	: 'post',
+					url		: '/products',
+					data	: formdata,
 					datatype: 'json',
 					
 						success	: function(data){
+							console.log(data)
 						document.getElementById('on_jumb').innerHTML = data.name+'<br>'+data.description+'<br>'+data.price;
 					}
 				})
