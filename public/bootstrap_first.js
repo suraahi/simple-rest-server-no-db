c83ae0c4509fffc06d719e4ddb40b676562e7b57
unknown <bhushan_indian@hotmail.com>
2015-03-04 11:30:57 -0500

    store exe (Tuesday-1)

--- public/bootstrap_first.js
+++ public/bootstrap_first.js
@@ -8,29 +8,54 @@
 			$(this).parent('div').remove();
 		}
 		})*/
-		$('.on_page').on('click',":button.gly",function(){
-			//console.log(this)
+			//$('.on_page').load("/products"){
+			//$.each(data,function(i,data){
+
+			//})
+
+		/*$('.on_page').on('click',"button",function(){
+			/*var but = $("button.gly").each(function(i,value){
+			console.log(i,value)
 			$(this).parent('div').remove();
-		})
-			//$('on_page').load(/products)
-			$('#buton').click(function(event){	
-			event.preventDefault()
+			})
+		});*/
+	$('.on_page').on('click',":button.gly",function(){
+		$(this).parent('div').remove();
+	});
+
+		$.getJSON( "/products", function(data){
+			$.each(data,function(i,data){
+				html = $('<div class="con container well col-md-8 pull-left"><button class=" gly glyphicon glyphicon-remove-sign pull-right"></button><button class="pri btn-info btn btn-sm pull-right">'+data.price+" $ "+'</button><div class="item">'+data.name+'</div><div class="des pull-left">'+data.description+'</div>')
+					$('.on_page').append(html);
+			})
+		});
+			//$('.on_page').load("/products")
+		$('.add').click(function(event){	
+		event.preventDefault()
+			var formdata ={
+			'name' 			: $('input[name=item]').val(),
+			'description'	: $('input[name=des]').val(),
+			'price'			: $('input[name=price]').val(),
+			}
+			console.log(formdata);
+			$.ajax({
+				type		: 'post',
+				url			: '/products',
+				data		: formdata,
+				datatype	: 'json',
 				
-				var formdata ={
-					'name' 			: $('input[name=item]').val(),
-					'description'	: $('input[name=des]').val(),
-					'price'			: $('input[name=price]').val(),
-				}
-				console.log(formdata);
-				$.ajax({
-					type	: 'post',
-					url		: '/products',
-					data	: formdata,
-					datatype: 'json',
+				success	: function(data){
 					
-						success	: function(data){
-							html = $('<div class="con container well col-md-8 pull-left"><button class=" gly glyphicon glyphicon-remove-sign pull-right"></button><button class="pri btn-info btn btn-sm pull-right">'+data.price+" $ "+'</button><div class="item">'+data.name+'</div><div class="des pull-left">'+data.description+'</div>')
-							$('.on_page').append(html);
+					html = $('<div class="con container well col-md-8 pull-left"><button class=" gly glyphicon glyphicon-remove-sign pull-right "id = '+data.id+'></button><button class="pri btn-info btn btn-sm pull-right">'+data.price+" $ "+'</button><div class="item">'+data.name+'</div><div class="des pull-left">'+data.description+'</div>')
+					console.log(data.id)
+					$('.on_page').append(html);
+
+				}
+			})
+		})			
+						
+	})
+							
 							/*$('.on_page').click(function(event){
 								
 								//alert("hello")
@@ -51,8 +76,3 @@
 							$(a).append(pri_val)
 							$('.on_page').append(a);*/
 
-					}
-				})
-			})			
-						
-	})
